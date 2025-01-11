423383a6bfb4ad7c7beab68bd19af121e542cf14
drlee1218 <52195835+drlee1218@users.noreply.github.com>
2025-01-11 14:51:06 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,5 +1,6 @@
 def solution(my_string):
     swap=my_string.lower()
     swaplist=list(swap)
-    swaplist.sort()
+    swaplist.sort() 
     return ''.join(swaplist)
+#sort는 리스트 자체를 정렬하고 sorted는 문자열, 리스트, 튜플 등 다양한 데이터 타입을 정렬한 뒤 이를 새로운 리스트로 반환한다.
