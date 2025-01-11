f9cb16ffec43dcae576183c3501822dd32c6bb29
drlee1218 <52195835+drlee1218@users.noreply.github.com>
2025-01-11 16:40:53 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -12,3 +12,7 @@ def solution(my_string):
         answer+=int(''.join(s))
         
     return answer
+#def solution(my_string):
+#   answer=0
+#   s=''.join(i if i.isdigit() else ' ' for i in my_string)
+#   return sum(int(i) for i in s.split())
