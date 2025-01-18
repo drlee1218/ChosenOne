7bb22a045c613d8a88c4403ea39d738ae7b642a5
drlee1218 <52195835+drlee1218@users.noreply.github.com>
2025-01-18 15:57:30 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -9,3 +9,8 @@ def solution(lines):
     
         
     return answer
+"""집합을 이용한 풀이
+def solution(lines):
+    sets = [set(range(min(l), max(l))) for l in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
+    """
