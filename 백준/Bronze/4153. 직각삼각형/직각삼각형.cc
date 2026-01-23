ce1f2b4dbd08c3222a3148942380356abc01a61b
drlee1218 <52195835+drlee1218@users.noreply.github.com>
2026-01-23 14:05:24 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    while(1)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+       
+        if(a==0&&b==0&&c==0)
+            break;
+        
+        if(b*b + c*c == a*a || a*a + c*c ==b*b || b*b + a*a==c*c)
+            printf("right\n");
+        else
+            printf("wrong\n");
+    }
+    return 0;
+}
