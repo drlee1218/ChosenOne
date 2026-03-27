20bc8c2f09b1e9a39a90ddeaf065ad17b60ea9e9
drlee1218 <52195835+drlee1218@users.noreply.github.com>
2026-03-28 00:16:51 +0900

    [level 2] Title: 타겟 넘버, Time: 366.39 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,20 @@
+def solution(numbers, target):
+    answer = 0
+    
+    def dfs(index, current_sum):
+        nonlocal answer
+        
+        # 모든 숫자를 다 사용했을 때
+        if index == len(numbers):
+            if current_sum == target:
+                answer += 1
+            return
+        
+        # + 선택
+        dfs(index + 1, current_sum + numbers[index])
+        
+        # - 선택
+        dfs(index + 1, current_sum - numbers[index])
+    
+    dfs(0, 0)
+    return answer
