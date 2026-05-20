d3795465d8c42e695b198a7e580f7f3c62e1fc83
drlee1218 <52195835+drlee1218@users.noreply.github.com>
2026-05-20 15:08:10 +0900

    [level 3] Title: 입국심사, Time: 268.81 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,19 +1,25 @@
 def solution(n, times):
     left = 1
-    right = max(times) * n  #최악의 경우
-    
+    right = max(times) * n #최악의 경우의 수
+
     answer = right
-    
+
     while left <= right:
         mid = (left + right) // 2
-        
-        #mid 시간 동안 처리 가능한 인원
-        people = sum(mid // time for time in times)
-        
+
+        # mid 시간 동안 처리 가능한 사람 수
+        people = 0
+
+        for t in times:
+            people += mid // t
+
+        # n명 이상 처리 가능
         if people >= n:
             answer = mid
-            right = mid - 1  
+            right = mid - 1
+
+        # 아직 부족
         else:
-            left = mid + 1   #시간 부족
-    
+            left = mid + 1
+
     return answer
